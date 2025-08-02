92006f9247eeeb35196a0615bfd00afe577b669c
Addydevs <Addywolday@gmail.com>
2025-08-02 17:47:10 -0500

    Ensure home screen route exists

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
