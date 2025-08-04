3c2b57c234ea1cf4710b489a56dd7ab2a8bfc4d0
Addydevs <Addywolday@gmail.com>
2025-08-03 20:34:52 -0500

    feat: add capture fit profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,8 +1,6 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import ProfileScreen from '@/components/ProfileScreen';
-import { PhotoContext } from './_layout';
 
 export default function ProfilePage() {
-  const { photos } = useContext(PhotoContext);
-  return <ProfileScreen photos={photos} />;
+  return <ProfileScreen />;
 }
